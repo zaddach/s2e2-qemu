8699e778c6a33e3b7a07cad3d3ad02b5693169dd
Jonas Zaddach <zaddach@eurecom.fr>
2014-11-03 10:01:13 +0100

    Renamed forgotten variables
    Renamed variables for architectures I did not compile previously.
    This can be squashed with the commit
    'Added explicit casts and renamed variables so that headers can be included from C++ code'.

--- linux-user/main.c
+++ linux-user/main.c
@@ -3460,7 +3460,7 @@ CPUArchState *cpu_copy(CPUArchState *env)
         cpu_breakpoint_insert(new_cpu, bp->pc, bp->flags, NULL);
     }
     QTAILQ_FOREACH(wp, &cpu->watchpoints, entry) {
-        cpu_watchpoint_insert(new_cpu, wp->vaddr, (~wp->len_mask) + 1,
+        cpu_watchpoint_insert(new_cpu, wp->virtaddr, (~wp->len_mask) + 1,
                               wp->flags, NULL);
     }
 #endif
